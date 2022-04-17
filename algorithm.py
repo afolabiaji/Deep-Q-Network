be0b1033be93c504012efc1f36fa7d4a0e7433d3
afolabiaji <af.ajibade@gmail.com>
2022-04-17 20:53:27 +0000

    more network changes

--- algorithm.py
+++ algorithm.py
@@ -2,10 +2,22 @@ import numpy as np
 from hyperparameters import *
 from collections import deque
 
+from model import QNetwork
+
 #Initialize replay memory D to capacity N
 replay_memory = deque(maxlen=MEMORY_SIZE)
+
 #Initialize action-value function Q with random weights h
+action_value_function = QNetwork()
+
 #Initialize target action-value function Q^ with weights h2 5 h
+target_function = QNetwork()
+
+#set model params to be same as previous model
+with torch.no_grad():
+    target_function.network = action_value_function.network
+
+
 # Create render fucntion to run on server
 render = lambda: plt.imshow(env.render(mode="rgb_array"))
 observation, info = env.reset(seed=42, return_info=True)
@@ -15,6 +27,8 @@ for episode in range(20):
         render()
         if time_step < NOOP_MAX:
             action = 0
+            observation, reward, done, info = env.step(action)
+            continue
         elif epsilon > np.random():
             action = Q_NETWORK(observation)
         else:
